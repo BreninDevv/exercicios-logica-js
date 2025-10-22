debab7924bf9a7b9c2ca08762aa4cf04e4420fdc
Breno Henrique <brenoeevil@gmail.com>
2025-10-22 17:16:20 -0300

    feat: Pasta de exercícios, conteúdo de variaveis e tipos de dados

---
+++ exercícios/variaveisTipos.js
@@ -0,0 +1,15 @@
+//VARIÁVEIS//
+let nome = "Breno";
+const idade = 19;
+let ativo = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof ativo);
+
+//TEMPLATE STRING//
+console.log(`Olá meu nome é ${nome}, tenho ${idade} anos!`);
+
+//CONVERSÃO DE TIPOS//
+const numero = "10";
+console.log(Number(numero) + 5);
